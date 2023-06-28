04ff37a9eea4c7a69930f94fc47d4475b9f6141b
cervecedin <sistemascorasur@gmail.com>
2023-06-28 17:41:36 -0500

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -19,26 +19,30 @@ function App() {
           <div className="d-flex flex-column ms-5">
 
             <div className="text-center">
-              <img src="../public/logo.png"
+              <img src="/logo.png"
                 style={{width: '385px', alignItems: 'center'}} alt="logo" />
             </div>
 
             <p style={{textAlign:'center', marginBottom:'15px'}}>Por favor complete los campos</p>
 
 
-            <MDBInput wrapperClass='mb-4' placeholder='Email address' id='form1' type='email' required style={{textAlign:'center', border:'1px solid black', marginBlock:'5px', marginLeft:'45%'}}/>
-            <MDBInput wrapperClass='mb-4' placeholder='Password' id='form2' type='password' required style={{textAlign:'center', border:'1px solid black', marginBlock:'5px', marginLeft:'45%'}}/>
+            <MDBInput wrapperClass='mb-4' placeholder='Email address' id='form1' type='email' required style={{textAlign:'center', border:'1px solid black', marginBlock:'5px', 
+            marginLeft:'45%'}}/>
+            <MDBInput wrapperClass='mb-4' placeholder='Password' id='form2' type='password' required style={{textAlign:'center', borderWidth:'2px', marginBlock:'5px', 
+            marginLeft:'45%',borderColor:'#ee7724, #d8363a, #dd3675, #b44593'}}/>
 
 
             <div className="text-center pt-1 mb-5 pb-1">
-              <MDBBtn className="gradient-custom-2">Sign in</MDBBtn><br></br><br></br>
+              <MDBBtn className="" style={{borderColor:'right, #ee7724, #d8363a, #dd3675, #b44593',borderStyle:'solid',borderWidth:'2px', color : 'black', padding:'7px'}}>
+                Ingresar</MDBBtn><br></br><br></br>
               <a className="text-muted" href="#!" style={{color:'#5d5d61', marginTop:'47px'}}>¿Has olvidado tu contraseña?</a>
             </div><br></br>
 
             <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4" style={{textAlign:'center'}}>
               <p className="mb-0">¿No tienes una cuenta?</p><br></br>
-              <MDBBtn outline className='mx-2' color='danger' >
-                CREAR
+              <MDBBtn outline className='mx-2' color='danger' style={{borderColor:'#ee7724, #d8363a, #dd3675, #b44593',borderStyle:'solid',borderWidth:'2px', color : 'black', 
+              padding:'7px'}}>
+                Crear
               </MDBBtn>
             </div>
 
